267f6a201841a5b1c58fc209887254b523b735b7
AKAPaneL <dpanzi@outlook.com>
2023-02-20 11:34:56 +0800

    初步使用

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
+
+
 </script>
 
 <template>
@@ -11,7 +13,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <HelloWorld />
 </template>
 
 <style scoped>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,36 +1,34 @@
-<script setup>
-import { ref } from 'vue'
-
-defineProps({
-  msg: String,
-})
-
+<script>
+import { ref,reactive } from 'vue'
 const count = ref(0)
+export default {
+  setup(){
+    const msg = ref({
+      id:'111111'
+    })
+    const obj = reactive({
+      name:'panel'
+    })
+    const setMsg = ()=>{
+      msg.value.id = 'hi panel'
+    }
+    const setName = ()=>{
+      obj.name = 'p'
+    }
+
+    return { setMsg,msg,obj,setName }
+    
+  }
+}
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
+  <h1>{{ msg.id }}</h1>
   <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
+    <button type="button" @click="setMsg"> {{ msg.id }}</button>
   </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <div>{{ obj.name }}</div>
+  <button @click="setName">set-name</button>
 </template>
 
 <style scoped>
